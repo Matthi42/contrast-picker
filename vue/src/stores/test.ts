ae959ef31e3b2baa3bf1c6fcfb658ec943982ff2
Matthi42 <matthi.altensteig@gmail.com>
2022-10-10 07:17:49 +0200

    feat: add the test-view

---
+++ vue/src/stores/test.ts
@@ -0,0 +1,28 @@
+import { defineStore } from "pinia";
+import { computed, reactive, watch } from "vue";
+
+// a Store for all users
+export const useTestStore = defineStore('test', () => {
+    // user array is first insatnceated empty
+    const users = reactive([] as User[])
+    // data from key-value store is loadet into store
+    Neutralino.storage.getData('users').then(v => {
+        Object.assign(users, reactive(JSON.parse(v) as object[]))
+    })
+    // if data changes the persistet values are updated
+    watch(users,async (newVal, oldVal) => {
+        await Neutralino.storage.setData('users',JSON.stringify(newVal))
+    }, { deep: true})
+
+    // getters and setters are normal pinia
+    const addUser = (user: User) => {
+        users.push(user)
+    }
+    
+    const userList = computed(() => users.map(u => ({ name: u.name, id: u.id })))
+
+    return {
+        userList,
+        addUser
+    }
+})
--- vue/src/views/cTest.vue
+++ vue/src/views/cTest.vue
@@ -1,6 +1,68 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+import ArrowButton from '../components/arrowButton.vue'
+import BigButton from '../components/bigButton.vue'
+import C from '../components/c.vue'
+import { useRoute } from 'vue-router'
+import { useColorStore } from '../stores/color'
+
+const route = useRoute()
+const colorStore = useColorStore()
+const arrows = ref([1,2,3,0,'c',4,7,6,5])
+
+const variant = colorStore.getColorVariantColorsByID(route.params.colorVariantID as string)
+
+const clickedArrow = (n: number) => {
+    clicks.value.push({actualRotation: 0, chosenRotation:n})
+}
+const clicks = ref([] as {actualRotation:number, chosenRotation:number}[])
+
+const currentSize = computed(() => `${100 * Math.pow(0.8, clicks.value.length)}px`)
 
 </script>
 <template>
-
+    <div class="container">
+        <div class="grid">
+            <div v-for="n in arrows" :style="{backgroundColor: n == 'c' ?  variant.background : ''}">
+                <ArrowButton v-if="n != 'c'" :rotation="(n as number)" @click="clickedArrow(n as number)"/>
+                <C v-if="n == 'c'" :fill="variant.foreground" :width="currentSize"/>
+            </div>
+        </div>
+        <div class="side">
+            <div>{{ currentSize }}</div>
+            <div>{{ clicks }}</div>
+            <BigButton>Pause</BigButton>
+        </div>
+    </div>
 </template>
+<style scoped lang="scss">
+.container{
+    display:grid;
+    grid-template-columns: 340px 600px 340px;
+    background-color: gray;
+    height: 720px;
+}
+.grid{
+    padding-top: 60px;
+    grid-column-start: 2;
+    display:grid;
+    grid-template-columns: 200px 200px 200px;
+    grid-template-rows: 200px 200px 200px;
+    div {
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 30px;
+    }
+}
+.side {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    button{
+        margin-bottom: 50px;
+        width:250px
+    }
+}
+</style>
