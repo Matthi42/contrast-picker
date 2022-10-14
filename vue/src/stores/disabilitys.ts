68fb7e2c03aea01e9b2e636561856a5f843bf6cc
Matthi42 <matthi.altensteig@gmail.com>
2022-10-14 07:37:48 +0200

    feat: list of disabilitiys are saved globally

---
+++ vue/src/stores/disabilitys.ts
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+import { reactive, watch } from "vue";
+
+export const useDisabilityStore = defineStore('disability', () => {
+    
+    const disabilitys = reactive([] as string[])
+
+    Neutralino.storage.getData('disabilitys').then(d => {
+        Object.assign(disabilitys, reactive(JSON.parse(d) as string[]))
+    }).catch((e) => {
+        console.error(e)
+    })
+
+    watch(disabilitys, async (newVal, oldVal) => {
+        await Neutralino.storage.setData('disabilitys', JSON.stringify(newVal))
+    }, {deep: true})
+
+    const setDisability = (disability: string) => {
+        if (!disabilitys.some(d => d === disability))
+            disabilitys.push(disability)
+    }
+
+    return {
+        disabilitys,
+        setDisability
+    }
+})
