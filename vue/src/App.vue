6f71fe0c80fabac547f8977c31448ad05d40c101
Matthi42 <matthi.altensteig@gmail.com>
2022-10-28 10:49:35 +0200

    feat: exit Button

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,13 +3,27 @@
   contains a router view that turns into every other view in ./views
 -->
 <script setup lang="ts">
+import X from './components/x.vue'
+
+const closeApp = () => {
+  Neutralino.app.exit()
+}
 
 </script>
 
 <template>
+  <div id="close">
+    <X height="45" @click="closeApp"/>
+  </div>
   <Suspense><RouterView /></Suspense>
 </template>
 
 <style scoped lang="scss">
-
+  #close {
+    position: absolute;
+    left:0;
+    top:0;
+    z-index: 3;
+    padding:20px;
+  }
 </style>
