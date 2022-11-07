5133e46dcdb42ecff2b32df98697ad04538aada5
Matthi42 <matthi.altensteig@gmail.com>
2022-11-07 08:09:14 +0100

    feat: add info-icon

---
+++ vue/src/components/i.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <svg  viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd"
+            d="M50 95C74.8528 95 95 74.8528 95 50C95 25.1472 74.8528 5 50 5C25.1472 5 5 25.1472 5 50C5 74.8528 25.1472 95 50 95ZM50 87C70.4345 87 87 70.4345 87 50C87 29.5655 70.4345 13 50 13C29.5655 13 13 29.5655 13 50C13 70.4345 29.5655 87 50 87Z"
+            fill="black" />
+        <circle cx="50" cy="27" r="5" fill="black" />
+        <rect x="45" y="37" width="10" height="40" rx="5" fill="black" />
+    </svg>
+</template>
