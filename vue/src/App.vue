f29c59449312d9feee9185dd92d34416902d06ac
Matthi42 <matthi.altensteig@gmail.com>
2022-11-07 08:05:21 +0100

    feat: make home button

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,16 +4,20 @@
 -->
 <script setup lang="ts">
 import X from './components/x.vue'
+import { useRoute } from 'vue-router';
 
 const closeApp = () => {
   Neutralino.app.exit()
 }
+
+const Route = useRoute()
 
 </script>
 
 <template>
   <div id="close">
     <X height="45" @click="closeApp"/>
+    <div>{{ Route.name}}</div>
   </div>
   <Suspense><RouterView /></Suspense>
 </template>
